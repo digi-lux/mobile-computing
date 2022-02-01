2760b3fcf30dbd726209fbc4de83f9257d2aa5c1
Andy Alorwu <aalorwu@volvocars.com>
2022-02-01 08:26:34 +0100

    show notifications when payment is made

--- app/src/main/java/com/codemave/mobilecomputing/ui/payment/PaymentViewModel.kt
+++ app/src/main/java/com/codemave/mobilecomputing/ui/payment/PaymentViewModel.kt
@@ -1,20 +1,35 @@
 package com.codemave.mobilecomputing.ui.payment
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.NotificationManagerCompat.from
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.codemave.mobilecomputing.Graph
+import com.codemave.mobilecomputing.R
 import com.codemave.mobilecomputing.data.entity.Category
 import com.codemave.mobilecomputing.data.entity.Payment
 import com.codemave.mobilecomputing.data.repository.CategoryRepository
 import com.codemave.mobilecomputing.data.repository.PaymentRepository
+import com.codemave.mobilecomputing.ui.home.categoryPayment.toDateString
+import com.codemave.mobilecomputing.util.NotificationWorker
+import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class PaymentViewModel(
     private val paymentRepository: PaymentRepository = Graph.paymentRepository,
-    private val categoryRepository: CategoryRepository = Graph.categoryRepository
+    private val categoryRepository: CategoryRepository = Graph.categoryRepository,
 ): ViewModel() {
     private val _state = MutableStateFlow(PaymentViewState())
 
@@ -22,15 +37,84 @@ class PaymentViewModel(
         get() = _state
 
     suspend fun savePayment(payment: Payment): Long {
+        createPaymentMadeNotification(payment)
         return paymentRepository.addPayment(payment)
     }
 
     init {
+        createNotificationChannel(context = Graph.appContext)
+        setOneTimeNotification()
         viewModelScope.launch {
             categoryRepository.categories().collect { categories ->
                 _state.value = PaymentViewState(categories)
             }
         }
+    }
+}
+
+private fun setOneTimeNotification() {
+    val workManager = WorkManager.getInstance(Graph.appContext)
+    val constraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.CONNECTED)
+        .build()
+
+    val notificationWorker = OneTimeWorkRequestBuilder<NotificationWorker>()
+        .setInitialDelay(10, TimeUnit.SECONDS)
+        .setConstraints(constraints)
+        .build()
+
+    workManager.enqueue(notificationWorker)
+
+    //Monitoring for state of work
+    workManager.getWorkInfoByIdLiveData(notificationWorker.id)
+        .observeForever { workInfo ->
+            if (workInfo.state == WorkInfo.State.SUCCEEDED) {
+                createSuccessNotification()
+            } else {
+                createErrorNotification()
+            }
+        }
+}
+
+private fun createNotificationChannel(context: Context) {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = "NotificationChannelName"
+        val descriptionText = "NotificationChannelDescriptionText"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
+            description = descriptionText
+        }
+        // register the channel with the system
+        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
+
+private fun createSuccessNotification() {
+    val notificationId = 1
+    val builder = NotificationCompat.Builder(Graph.appContext, "CHANNEL_ID")
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .setContentTitle("Success! Download complete")
+        .setContentText("Your countdown completed successfully")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+    with(NotificationManagerCompat.from(Graph.appContext)) {
+        //notificationId is unique for each notification that you define
+        notify(notificationId, builder.build())
+    }
+}
+
+private fun createPaymentMadeNotification(payment: Payment) {
+    val notificationId = 2
+    val builder = NotificationCompat.Builder(Graph.appContext, "CHANNEL_ID")
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .setContentTitle("New payment made")
+        .setContentText("You paid $${payment.paymentAmount} on ${payment.paymentDate.toDateString()}")
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+    with(NotificationManagerCompat.from(Graph.appContext)) {
+        notify(notificationId, builder.build())
     }
 }
 
